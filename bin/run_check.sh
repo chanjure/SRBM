43db440714fb99e36fa64b186ed3674f62db02a5
Chanju Park <chanju.b.park@gmail.com>
2023-04-25 16:50:46 +0100

    Added MNIST example

--- bin/run_check.sh
+++ bin/run_check.sh
@@ -1,6 +1,15 @@
 jupyter nbconvert --to notebook --execute notebook/SRBM_unsup_gen.ipynb
 mv notebook/SRBM_unsup_gen.nbconvert.ipynb notebook/SRBM_unsup_gen.ipynb
 nbstripout notebook/SRBM_unsup_gen.ipynb
+
+jupyter nbconvert --to notebook --execute notebook/SRBM_sup_gen_scalar_field.ipynb
+mv notebook/SRBM_sup_gen_scalar_field.nbconvert.ipynb notebook/SRBM_sup_gen_scalar_field.ipynb
+nbstripout notebook/SRBM_sup_gen_scalar_field.ipynb
+
+jupyter nbconvert --to notebook --execute notebook/SRBM_sup_gen_MNIST.ipynb
+mv notebook/SRBM_sup_gen_MNIST.nbconvert.ipynb notebook/SRBM_sup_gen_MNIST.ipynb
+nbstripout notebook/SRBM_sup_gen_MNIST.ipynb
+
 rm ./notebook/*.npz
 
 pytest -v
