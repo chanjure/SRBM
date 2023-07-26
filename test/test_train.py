1a6e2245ffcbd99bb048669aa2e1465703d0f1db
chanjure <chanjure@snu.ac.kr>
2023-07-26 16:01:46 +0100

    updated pytest accordingly

--- test/test_train.py
+++ test/test_train.py
@@ -92,7 +92,7 @@ def test_scale(m_scheme):
 
     K_phi_tc = torch.DoubleTensor(K_phi)
 
-    history = rbm.unsup_fit(K_phi_tc, S_fast,\
+    history = rbm.unsup_fit(K_phi_tc,\
             epochs=epochs, lr=lr, beta=0.5, l2=1e-1, batch_size=batch_size,\
             verbose=True, lr_decay=0.99)
 
